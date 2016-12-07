2c3b0a81ee59641fcf2040dc7a817be49e50e750
David Stainton <dstainton415@gmail.com>
2016-12-07 22:28:01 +0000

    Get rid of olde crypto primitives
    here we remove the SHA256 hash and hashmac as well as
    AES counter mode stream cipher and it's lioness friend.

--- sphinxmixcrypto/params.py
+++ sphinxmixcrypto/params.py
@@ -26,15 +26,12 @@ used to encrypt/decrypt sphinx mixnet packets.
 import os
 from functools import reduce
 
-from Crypto.Cipher import AES
-from Crypto.Hash import SHA256, HMAC
-from Crypto.Util import number
 from Crypto.Util.strxor import strxor
 from nacl.bindings import crypto_scalarmult
 from pyblake2 import blake2b
 from Cryptodome.Cipher import ChaCha20
 
-from pylioness import Chacha20_Blake2b_Lioness, AES_SHA256_Lioness
+from pylioness import Chacha20_Blake2b_Lioness
 
 from sphinxmixcrypto.nym_server import Nymserver
 from sphinxmixcrypto.node import KeyMismatchError, BlockSizeMismatchError
@@ -45,50 +42,6 @@ RHO_HASH_PREFIX = b'\0x22'
 MU_HASH_PREFIX = b'\0x33'
 PI_HASH_PREFIX = b'\0x44'
 TAU_HASH_PREFIX = b'\0x55'
-
-
-class GroupP:
-    "Group operations mod p"
-
-    def __init__(self):
-        # A 2048-bit prime
-        self.__p = 1913410438251547134012138308293430882878846516487692248301804666518956860838 + \
-            533652138552845585287022672941951578244576994631152454340178067976378738872954718198973 + \
-            706028940706247921401744642825115746994081956867321580573181552152352900883786890992958 + \
-            562877467321623953640627020158543955913969169796635999051041203446136976835775661506057 + \
-            517706067943361819659545828482653492891104587913554024076544568803664876176841762410041 + \
-            643804280840775935598361131923601799147307296410539233589716020166265519420170231237267 + \
-            848121356044355838177752128425942891191400809793668864920967000989279066999182347251553 + \
-            7714171774700422727
-
-        # A 256-bit prime.  q | p-1, and (p-1)/(2q) is also prime
-        self.__q = 106732665057690615308701680462846682779480968671143352109289849544853387479559
-
-        # A generator of the 256-bit subgroup of order q
-        self.generator = 484139441786349441222753937359181507222186847483440700310896462165694808760753 + \
-            313201440620938426400186061400541347047499861859506375079830182634177422300847 + \
-            601840574360281485737847061474817405657249365598958655758739651134727647466577 + \
-            884569940693579983363636508320621833059331551372071146035325524395420417805763 + \
-            312260922194735482986906987547422160345740734733202920357368017078519121268583 + \
-            377382750037104414214664818336930092771460011453820969206987379419171538261727 + \
-            876814959465431589529648553329257486681938507314187048365957770789256545184218 + \
-            1763727355979252885729688362656338077037492411991956527093735651034592
-
-    def gensecret(self):
-        return number.bytes_to_long(os.urandom(256)) % self.__q
-
-    def expon(self, base, exp):
-        return pow(base, exp, self.__p)
-
-    def multiexpon(self, base, exps):
-        return pow(base, reduce(lambda x, y: x * y % self.__q, exps), self.__p)
-
-    def makeexp(self, data):
-        return number.bytes_to_long(data) % self.__q
-
-    def in_group(self, alpha):
-        return alpha > 1 and alpha < (self.__p - 1) and \
-            pow(alpha, self.__q, self.__p) == 1
 
 
 class GroupECC:
@@ -142,17 +95,6 @@ class GroupECC:
         return len(alpha) == self.size
 
 
-def SHA256_hash(data):
-    h = SHA256.new()
-    h.update(data)
-    return h.digest()
-
-
-def SHA256_hash_mac(key, data, digest_size=16):
-    m = HMAC.new(key, msg=data, digestmod=SHA256)
-    return m.digest()[:digest_size]
-
-
 def Blake2_hash(data):
     b = blake2b(data=bytes(data))
     h = b.digest()
@@ -164,37 +106,10 @@ def Blake2_hash_mac(key, data, digest_size=16):
     return b.digest()
 
 
-def AES_stream_cipher(key):
-    class xcounter:
-        def __init__(self, size):
-            self.i = 0
-            self.size = size
-
-        def __call__(self):
-            if self.i > 2**self.size:
-                raise Exception("AES_stream_cipher counter exhausted.")
-            ii = number.long_to_bytes(self.i)
-            ii = b'\x00' * (self.size - len(ii)) + ii
-            self.i += 1
-            return ii
-    return AES.new(key, AES.MODE_CTR, counter=xcounter(16))
-
-
 def Chacha20_stream_cipher(key):
     b = blake2b(data=key)
     new_key = b.digest()
     return ChaCha20.new(key=new_key[8:40], nonce=new_key[0:8])
-
-
-class AES_Lioness:
-    def __init__(self, key, block_size):
-        self.cipher = AES_SHA256_Lioness(key, block_size)
-
-    def encrypt(self, block):
-        return self.cipher.encrypt(block)
-
-    def decrypt(self, block):
-        return self.cipher.decrypt(block)
 
 
 class Chacha_Lioness:
